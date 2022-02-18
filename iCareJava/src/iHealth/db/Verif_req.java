908cacf2171dc41590f3ad7bcdc24c4978da3c98
Merry <zenati.meriem.10@gmail.com>
2022-02-18 13:48:09 +0100

    [DB] Vérification des requêtes
    Création du Main permettant de vérifier nos requêtes.

---
+++ iCareJava/src/iHealth/db/Verif_req.java
@@ -0,0 +1,52 @@
+package iHealth.db;
+
+import iHealth.db.DatabaseAccessProperties;
+import iHealth.db.SQLWarningsExceptions;
+import iHealth.db.requetes;
+import iHealth.nf.Patient;
+import static iHealth.nf.Sexe.FEMME;
+import static iHealth.nf.Sexe.HOMME;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author MZ
+ */
+public class Verif_req {
+
+private static final String configurationFile = "src/database.properties";
+
+    public static void main(String args[]) {
+        try {
+            String jdbcDriver, dbUrl, username, password;
+            DatabaseAccessProperties dap = new DatabaseAccessProperties(configurationFile);
+            jdbcDriver = dap.getJdbcDriver();
+            dbUrl = dap.getDatabaseUrl();
+            username = dap.getUsername();
+            password = dap.getPassword();
+// Load the database driver
+            Class.forName(jdbcDriver);
+// Get a connection to the database
+            Connection conn = DriverManager.getConnection(dbUrl, username, password);
+            Patient patient = new Patient("210000258","GUNNESS","Belle","01/01/2001",FEMME,"adresse1");
+            requetes.getDMA_IPP(conn,patient);
+            
+// Print information about connection warnings
+            SQLWarningsExceptions.printWarnings(conn);
+            conn.close();
+            
+        } catch (SQLException se) {
+// Print information about SQL exceptions
+            SQLWarningsExceptions.printExceptions(se);
+            return;
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+    }    }
+   
